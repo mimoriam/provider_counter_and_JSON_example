0fb853b0b6e2358d4869f790d87bf7528f4edc98
Mimoriam <mimoriam420@gmail.com>
2021-05-13 08:09:44 +0500

    Initial commit

---
+++ lib/state/root.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_counter_example/main.dart';
+
+// Step 1: Create a class model with ChangeNotifier having all the properties and methods for state in it:
+class Counter extends ChangeNotifier {
+  int count = 0;
+
+  void increment() {
+    count++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    count--;
+    notifyListeners();
+  }
+}
+
+class Root extends StatefulWidget {
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  @override
+  Widget build(BuildContext context) {
+    return MyHomePage();
+  }
+}
